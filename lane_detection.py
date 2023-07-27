af0ae815f8d5d50d942c63b649663672bfea4b74
Charlotte <charlottefreedman9898@gmail.com>
2023-07-27 16:59:44 -0400

    things

--- lane_detection.py
+++ lane_detection.py
@@ -7,24 +7,26 @@ def get_x_int(elem):
 
 def detect_lines(img, threshold1=50, threshold2=150, apertureSize=3, 
                  minLineLength=100, maxLineGap=10):
+    img = cv2.GaussianBlur(img, (9, 9), 0)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # convert to grayscale
     edges = cv2.Canny(gray, threshold1, threshold2, apertureSize) # detect edges
     lines = cv2.HoughLinesP(
-                edges,
-                rho = 1,
-                theta = np.pi/180,
-                threshold = 100,
-                minLineLength=minLineLength,
-                maxLineGap=maxLineGap
-        ) # detect lines
+            edges,
+            rho = 1,
+            theta = np.pi/180,
+            threshold = 100,
+            minLineLength = minLineLength,
+            maxLineGap = maxLineGap,
+    ) # detect lines
     return lines
 
 def draw_lines(img, lines, color=(0, 102, 255)):
+    temp_img = img
     for line in lines:
         x1, y1, x2, y2 = line[0]
-        cv2.line(img, (x1, y1), (x2, y2), color, 2)
+        cv2.line(temp_img, (x1, y1), (x2, y2), color, 2)
 
-    return img
+    return temp_img
 
 def get_slopes_intercepts(lines): #only X-intercept
     slopes = []
@@ -47,6 +49,8 @@ def get_slopes_intercepts(lines): #only X-intercept
     return (slopes, xInts)
 
 def detect_lanes(lines):
+
+    #merge lines
     lanes = []
     for line in lines:
         x1, y1, x2, y2 = line[0]
@@ -67,39 +71,48 @@ def detect_lanes(lines):
     for line in lanes:
         canAdd = True
         for cleanedLine in cleanedLines:
-            if abs(cleanedLine[1] - line[1]) < 0.1:
+            if abs(cleanedLine[0] - line[0]) < 0.1:
                 canAdd = False
 
         if canAdd:
             cleanedLines.append(line)
 
-    sorted_lines = []
+    #lines merged
+
     sorted_lines = cleanedLines
     sorted_lines.sort(key=get_x_int)
+    if len(sorted_lines) > 2:
+        dist1 = abs(sorted_lines[1][1] - sorted_lines[0][1])
+        dist2 = abs(sorted_lines[2][1] - sorted_lines[1][1])
 
-    dist1 = abs(sorted_lines[1][1] - sorted_lines[0][1])
-    dist2 = abs(sorted_lines[2][1] - sorted_lines[1][1])
+        if dist1 > dist2:
+            sorted_lines.pop(0)
+        if len(sorted_lines) % 2 != 0:
+            sorted_lines.pop(len(sorted_lines) - 1)
+
+    #pair lines
     pairs = []
-
-    if dist1 > dist2:
-        sorted_lines.pop(0)
-    if len(sorted_lines) % 2 != 0:
-        sorted_lines.pop(len(sorted_lines) - 1)
     if len(sorted_lines) >= 2:
         for i in range(0, len(sorted_lines)-1, 2):
             pairs.append([sorted_lines[i], sorted_lines[i+1]])
-
-    return pairs
+    if len(pairs) > 0:
+        if len(pairs) % 2 == 0:
+            center_lane = pairs[int((len(pairs))/2) -1]
+        else:
+            center_lane = pairs[int((len(pairs) + 1)/2) -1]
+    else:
+        return []
+    
+    return center_lane
 
 def draw_lanes(img, lanes):
     temp = img
-    for lines in lanes:
-        for line in lines:
-            x1 = line[2]
-            y1 = line[3]
-            x2 = line[4]
-            y2 = line[5]
-            cv2.line(img, (x1, y1), (x2, y2), (0, 102, 255), 2)
+    for line in lanes:
+        x1 = line[2]
+        y1 = line[3]
+        x2 = line[4]
+        y2 = line[5]
+        cv2.line(img, (x1, y1), (x2, y2), (0, 102, 255), 2)
     return temp
 
         
